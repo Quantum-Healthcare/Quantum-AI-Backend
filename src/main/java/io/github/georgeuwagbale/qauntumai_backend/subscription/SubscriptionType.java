ff9a412f9a5ce847d1a5ca54c22aa8e42e973326
georgeuwagbale <uwagbalegeorge@gmail.com>
2023-12-13 14:50:06 +0100

    Add Subscription module Add Appointment module

--- src/main/java/io/github/georgeuwagbale/qauntumai_backend/subscription/SubscriptionType.java
+++ src/main/java/io/github/georgeuwagbale/qauntumai_backend/subscription/SubscriptionType.java
@@ -1,8 +1,10 @@
 package io.github.georgeuwagbale.qauntumai_backend.subscription;
 
 public enum SubscriptionType {
+
     FREE(0),
     PREMIUM(1000);
 
-    SubscriptionType(long i) {}
+    SubscriptionType(long i) {
+    }
 }
